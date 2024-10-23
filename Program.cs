06390a94c881b2a1b5ff4574b118b50f881b8e8d
Anthony-Flowers-Student <anfla37@student.oaisd.org>
2024-10-23 18:06:11 +0000

    made all functions

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       
+    }
+    public static void Write1ThroughN_While()
+    {
+
+    }
+    public static void Write1ThroughN_For()
+    {
+
+    }
+
+    public static void WriteNThrough1_While()
+    {
+
+    }
+    public static void WriteNThrough1_For()
+    {
+
+    }
+    public static void WriteEvensThrough100()
+    {
+
+    }
+    public static void FindSum()
+    {
+
+    }
+    public static void FindSumOfEvenNumbers()
+    {
+
+    }
+    public static void FindSumOfOddNumbers()
+    {
+
+    }
+    public static void OutputRightTriangle()
+    {
+
+    }
+    public static void OutOfRangeMessage()
+    {
+        
+    }
+}
+
