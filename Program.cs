82fa7ec0aed190739b519f5d521d49aeabbd1bee
Anthony-Flowers-Student <anfla37@student.oaisd.org>
2024-11-04 16:53:36 +0000

    Recommi since it was not fully commited. Includes the updates mentioned in the last commit.

--- Program.cs
+++ Program.cs
@@ -8,51 +8,419 @@ public class Program
 
     public static void Main(string[] args)
     {
+        Console.Clear();
+        Console.WriteLine("Write1ThroughN_While");
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("Write1ThroughN_While(-1)");
+        Write1ThroughN_While(-1);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("Write1ThroughN_While(5)");
+        Write1ThroughN_While(5);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("Write1ThroughN_While(0)");
+        Write1ThroughN_While(0);
+         Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("Press enter to move to next method.");
+        Console.ReadLine();
+        Console.Clear();
 
+        
+        Console.WriteLine("Write1ThroughN_For");
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("Write1ThroughN_For(-1)");
+        Write1ThroughN_For(-1);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("Write1ThroughN_For(5)");
+        Write1ThroughN_For(5);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("Write1ThroughN_For(0)");
+        Write1ThroughN_For(0);
+         Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("Press enter to move to next method.");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("WriteNThrough1_While");
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("WriteNThrough1_While(-1)");
+        WriteNThrough1_While(-1);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("WriteNThrough1_While(5)");
+        WriteNThrough1_While(5);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("WriteNThrough1_While(0)");
+        WriteNThrough1_While(0);
+         Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("Press enter to move to next method.");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("WriteNThrough1_For");
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("WriteNThrough1_For(-1)");
+        WriteNThrough1_For(-1);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("WriteNThrough1_For(5)");
+        WriteNThrough1_For(5);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("WriteNThrough1_For(0)");
+        WriteNThrough1_For(0);
+         Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("Press enter to move to next method.");
+        Console.ReadLine();
+        Console.Clear();
+
+         Console.WriteLine("WriteEvensThrough100");
+        WriteEvensThrough100();
+         Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("Press enter to move to next method.");
+        Console.ReadLine();
+        Console.Clear();
+        
+         Console.WriteLine("FindSum");
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("FindSum(5)");
+        FindSum(5);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSum(1)");
+        FindSum(1);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSum(-3)");
+        FindSum(-3);
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSum(100)");
+        FindSum(100);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Press enter to move to next method.");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("FindSumOfEvenNumbers");
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("FindSumOfEvenNumbers(10)");
+        FindSumOfEvenNumbers(10);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSumOfEvenNumbers(-5)");
+        FindSumOfEvenNumbers(-5);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSumOfEvenNumbers(2)");
+        FindSumOfEvenNumbers(2);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSumOfEvenNumbers(0)");
+        FindSumOfEvenNumbers(0);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine("FindSumOfEvenNumbers(1)");
+        FindSumOfEvenNumbers(1);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine("FindSumOfEvenNumbers(25)");
+        FindSumOfEvenNumbers(25);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine("FindSumOfEvenNumbers(-25)");
+        FindSumOfEvenNumbers(-25);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Press enter to move to next method.");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("FindSumOfOddNumbers");
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("FindSumOfOddNumbers(10)");
+        FindSumOfOddNumbers(10);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSumOfOddNumbers(-5)");
+        FindSumOfOddNumbers(-5);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSumOfOddNumbers(1)");
+        FindSumOfOddNumbers(1);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("FindSumOfOddNumbers(2)");
+        FindSumOfOddNumbers(2);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine("FindSumOfOddNumbers(25)");
+        FindSumOfOddNumbers(25);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine("FindSumOfOddNumbers(-25)");
+        FindSumOfOddNumbers(-25);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Press enter to move to next method.");
+        Console.ReadLine();
+        Console.Clear();
+
+        Console.WriteLine("OutputRightTriangle");
+        Console.WriteLine();
+        Thread.Sleep(1500);
+        Console.WriteLine("OutputRightTriangle(4)");
+        OutputRightTriangle(4);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("OutputRightTriangle(6)");
+        OutputRightTriangle(6);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("OutputRightTriangle(-4)");
+        OutputRightTriangle(-4);
+        Thread.Sleep(1500);
+        Console.WriteLine();
+        Console.WriteLine("OutputRightTriangle(-6)");
+        OutputRightTriangle(-6);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine("OutputRightTriangle(0)");
+        OutputRightTriangle(0);
+         Thread.Sleep(1500);
+         Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Press enter to end program.");
+        Console.ReadLine();
+        Console.Clear();
     }
     public static void Write1ThroughN_While(int n)
     {
-               while (n > 1)
+        if (n < 1)
+        {
+            OutOfRangeMessage(n);
+        }
+        int counter = 1;
+        while (counter <= n)
        {
-        Console.WriteLine(n + "")
+        Console.Write(counter + " ");
+        counter++;
        }
     }
-    public static void Write1ThroughN_For()
+    public static void Write1ThroughN_For(int n)
     {
-
+          if (n < 1)
+        {
+            OutOfRangeMessage(n);
+        }
+        for (int i = 1; i <= n; i++)
+        {
+        Console.Write(i + " ");
+       
+        }
     }
 
-    public static void WriteNThrough1_While()
+    public static void WriteNThrough1_While(int n)
     {
-
+        int counter = n;
+         if (n < 1)
+        {
+            OutOfRangeMessage(n);
+        }
+        while (counter <= n && counter > 0)
+       {
+        Console.Write(counter + " ");
+        counter--;
+        
+       }
     }
-    public static void WriteNThrough1_For()
+    public static void WriteNThrough1_For(int n)
     {
-
+           if (n < 1)
+        {
+            OutOfRangeMessage(n);
+        }
+         for (int i = n; i <= n && i > 0; i--)
+        {
+        Console.Write(i + " ");
+        }
     }
     public static void WriteEvensThrough100()
     {
-
+        int counter = 1;
+        while (counter <= 100)
+        {
+            if (counter % 2 == 0)
+            {
+            Console.WriteLine(counter);
+            }
+            counter++;
+            continue;
+        }
     }
-    public static void FindSum()
+    public static void FindSum(int n)
     {
-
+         int counter = 1;
+         int total = 0;
+        if (n == 0)
+        {
+            OutOfRangeMessage(n);
+        }
+        else if (n >= 1)
+        {
+        while (counter <= n)
+        {
+            total = counter + total;
+            counter++;
+            continue;
+        }
+        }
+        else if (n < 0) 
+        {
+             while (counter >= n)
+        {
+            total = counter + total;
+            counter--;
+            continue;
+        }
+        }
+        Console.Write(total);
     }
-    public static void FindSumOfEvenNumbers()
+    public static void FindSumOfEvenNumbers(int n)
     {
-
+         int counter = 0;
+         int total = 0;
+        
+        if (n >= 0)
+        {
+        while (counter <= n)
+        {
+            if (counter % 2 == 0)
+            {
+            total = counter + total;
+            }
+            if (counter == n)
+            {
+                Console.WriteLine(total);
+            }
+            counter++;
+            continue;
+        }
+        }
+        else if (n < 0)
+        {
+             while (counter >= n)
+        {
+            if (counter % 2 == 0)
+            {
+            total = counter + total;
+            }
+            if (counter == n)
+            {
+                Console.WriteLine(total);
+            }
+            counter--;
+            continue;
+        }
+        }
     }
-    public static void FindSumOfOddNumbers()
+    public static void FindSumOfOddNumbers(int n)
     {
-
+         int counter = 1;
+         int total = 0;
+       
+        if (n >= 0)
+        {
+        while (counter <= n)
+        {
+            if (counter % 2 != 0)
+            {
+            total = counter + total;
+            }
+            if (counter == n)
+            {
+                Console.WriteLine(total);
+            }
+            counter++;
+            continue;
+        }
+        }
+        else if (n < 0)
+        {
+             while (counter >= n)
+        {
+            if (counter % 2 != 0)
+            {
+            total = counter + total;
+            }
+            if (counter == n)
+            {
+                Console.WriteLine(total);
+            }
+            counter--;
+            continue;
+        }
+        }
     }
-    public static void OutputRightTriangle()
+    public static void OutputRightTriangle(int _base)
     {
-
+        if (_base == 0)
+        {
+            OutOfRangeMessage(0);
+        }
+        if (_base > 0)
+        {
+            for (int i = 1; _base >= i; i++ )
+            {
+                for (int j = 0; j < i;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        else if (_base < 0)
+        {
+              for (int i = -1; _base <= i; i-- )
+            {
+                for (int j = _base; j <= i;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
     }
-    public static void OutOfRangeMessage()
+    public static void OutOfRangeMessage(int n)
     {
-
+        Console.WriteLine(n + " is not in the number range");
     }
 }
 
